54da5af172da37edee8e71866a326b4d83429533
zzp <zzptom@163.com>
2019-05-30 17:02:23 +0800

    Initial Commit

---
+++ hellowGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hellowGithub
+//
+//  Created by zzp-Mac on 2019/5/30.
+//  Copyright © 2019年 zzp-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hellowGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hellowGithub
+//
+//  Created by zzp-Mac on 2019/5/30.
+//  Copyright © 2019年 zzp-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
